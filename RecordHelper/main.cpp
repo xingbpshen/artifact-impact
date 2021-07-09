ff07a29a1dc5a9b42bcfea130ebc821c52c19c9d
AntonioShen <1053619500@qq.com>
2021-07-09 17:20:38 +0800

    Minor updated.

--- RecordHelper/main.cpp
+++ RecordHelper/main.cpp
@@ -3,17 +3,20 @@
 
 using namespace std;
 
+string set_base_address();
+
 int main() {
     std::cout << "Welcome to the Artifact Impact data record helper!" << std::endl;
 
+    //ofstream record_file ("../../Data/bing_feng/0.csv", ios::app);
+    string base_address = set_base_address();
     
-    ofstream record_file ("../../Data/bing_tao/0.csv", ios::app);
     //record_file << "h\n";
-    record_file.close();
+    //record_file.close();
     return 0;
 }
 
-string set_Base_Address(){
+string set_base_address(){
     std::string record_address_base = "../../Data/";
     std::cout << "Please enter the number as the set of the artifact (0: JueDouShi, 1: YueTuan, 2: MoNv, 3: QianYan, 4: ShaoNv, "
                  "5: CuiLv, 6: DuHuo, 7: ChenLun, 8: CangBai, 9: BingFeng):" << std::endl;
