35cf133acd4762eee5810e1a3f84b834a74b7517
AntonioShen <1053619500@qq.com>
2021-07-09 22:37:41 +0800

    Minor updated.

--- RecordHelper/main.cpp
+++ RecordHelper/main.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 string set_base_address();
+void before_enhancement();
 
 int main() {
     std::cout << "Welcome to the Artifact Impact data record helper!" << std::endl;
 
-    //record_file << "h\n";
-    //record_file.close();
+    before_enhancement();
     return 0;
 }
 
@@ -22,7 +22,7 @@ void before_enhancement(){
     before_address.append("0.csv");
 
     cout << "Please enter the number as the type of the artifact "
-            "(0: Flower, 1: Plume, 2: Sands, 3: Goblet, 4: Circlet):" << endl;
+            "\n0: Flower, 1: Plume, 2: Sands, 3: Goblet, 4: Circlet\n>>";
     int type;
     cin >> type;
     ofstream record_file (before_address, ios::app);
@@ -30,7 +30,12 @@ void before_enhancement(){
     record_file << ",";
 
     cout << "Please enter the number as the type of the base attribute of the artifact "
-            "(0: Flower, 1: Plume, 2: Sands, 3: Goblet, 4: Circlet):" << endl;
+            "\n0: ATK, 1: ATK%, \n2: DEF, 3: DEF%, \n4: HP, 5: HP%, \n6: CRITRATE%, 7: CRITDMG%, \n8: ELEMASTER, 9: ENERREC%, "
+            "\n10: HEALBOU%, \n11: PHYDMG%, 12: PYRODMG%, 13: ELECTRODMG%, 14: CRYODMG%, \n15: HYDRODMG%, 16: ANEMODMG%, 17: GEODMG%\n>>";
+    cin >> type;
+    record_file << type;
+    record_file << ",";
+    record_file.close();
 
     ask_attrib_and_write(before_address);
 }
@@ -38,8 +43,12 @@ void before_enhancement(){
 void ask_attrib_and_write(string address){
     ofstream record_file (address, ios::app);
     float value;
-    string attrib_keywords[18]
-    value
+    string attrib_keywords[18] = {"ATK", "ATK%", "DEF", "DEF%", "HP", "HP%", "CRITRATE%", "ELEMASTER", "ENERREC%",
+                                  "HEALBOU%", "PHYDMG%", "PYRODMG%", "ELECTRODMG%", "CRYODMG%", "HYDRODMG%", "ANEMODMG%", "GEODMG%"};
+    cout << attrib_keywords->length();
+    for(int i = 0; i < attrib_keywords->size(); i++){
+
+    }
 }
 
 float ask_and_return(string prompt){
@@ -57,8 +66,8 @@ void write_to_file(string address, float value){
 string set_base_address(){
     std::string record_address_base = "../../Data/";
     std::cout << "Please enter the number as the set of the artifact "
-                 "(0: JueDouShi, 1: YueTuan, 2: MoNv, 3: QianYan, 4: ShaoNv, "
-                 "5: CuiLv, 6: DuHuo, 7: ChenLun, 8: CangBai, 9: BingFeng):" << std::endl;
+                 "\n0: JueDouShi, \n1: YueTuan, \n2: MoNv, \n3: QianYan, \n4: ShaoNv, "
+                 "\n5: CuiLv, \n6: DuHuo, \n7: ChenLun, \n8: CangBai, \n9: BingFeng\n>>";
     int set_num;
     std::cin >> set_num;
     switch(set_num){
